d7b4bf8a318649aa963f8157f43a8b8997f99c37
JuanJo Rey <juanreyfotos@gmail.com>
2021-08-28 13:46:51 +0200

    error

---
+++ src/main/java/com/example/error/app/controllers/AppController.java
@@ -0,0 +1,16 @@
+package com.example.error.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+	@SuppressWarnings("unused")
+	@GetMapping("/index")
+	public String index() {
+		Integer valor = 100/0;
+		return "index";
+	}
+	
+}
